2282431e1c472a52241ddd78ee90f4209633a5f8
Ilan Uzan <ilan9uzan@gmail.com>
2024-04-21 13:54:37 +0200

    got postgres tests working as well

--- run_tests.sh
+++ run_tests.sh
@@ -5,9 +5,10 @@ set -ex
 destroy() { docker-compose down --volumes; }
 trap destroy EXIT
 
+source .env
 docker-compose up --detach --build --force-recreate --remove-orphans
-tests_container_id=$(docker ps -aqf "name=plugin-tests")
-tests_exit_code=$(docker wait plugin-tests)
+tests_container_id=$(docker ps -aqf "name=${TESTS_CONTAINER_NAME}")
+tests_exit_code=$(docker wait "${TESTS_CONTAINER_NAME}")
 
 # by container_id since the container is dead
 docker logs --timestamps "${tests_container_id}" > tests.log && cat tests.log
