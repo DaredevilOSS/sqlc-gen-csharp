22760f1c32d8f039b9d3d8bb078fb2c40aea59c2
Doron Eli Rachman <doron050@gmail.com>
2024-05-09 23:31:13 +0300

    test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -2,9 +2,8 @@
 
 if [ "$GITHUB_ACTIONS" = "true" ]; then
     echo "Running in Github Actions"
-    echo "script TESTS_CONTAINER_NAME is ${TESTS_CONTAINER_NAME}"
-    tests_container_id=$(docker ps -aqf "name=plugin-tests")
-    tests_exit_code=$(docker wait "plugin-tests")
+    tests_container_id=$(docker ps -aqf "name=${TESTS_CONTAINER_NAME}")
+    tests_exit_code=$(docker wait "${TESTS_CONTAINER_NAME}")
     docker logs --timestamps "${tests_container_id}"
 else
     echo "Running in local"
