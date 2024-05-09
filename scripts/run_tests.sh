b075318f75a500ed7b75bda4aa1ca589a5070754
Doron Eli Rachman <doron050@gmail.com>
2024-05-09 18:21:31 +0300

    test

--- scripts/run_tests.sh
+++ scripts/run_tests.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
 
-set -ex
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+    tests_container_id=$(docker ps -aqf "name=${TESTS_CONTAINER_NAME}")
+    tests_exit_code=$(docker wait "${TESTS_CONTAINER_NAME}")
+else
+    set -ex
 
-destroy() { docker-compose down --volumes; }
-trap destroy EXIT
+    destroy() { docker-compose down --volumes; }
+    trap destroy EXIT
 
-source .env
-docker-compose up --detach --build --force-recreate --remove-orphans
-tests_container_id=$(docker ps -aqf "name=${TESTS_CONTAINER_NAME}")
-tests_exit_code=$(docker wait "${TESTS_CONTAINER_NAME}")
+    source .env
+    docker-compose up --detach --build --force-recreate --remove-orphans
+    tests_container_id=$(docker ps -aqf "name=${TESTS_CONTAINER_NAME}")
+    tests_exit_code=$(docker wait "${TESTS_CONTAINER_NAME}")
 
-# by container_id since the container is dead
-docker logs --timestamps "${tests_container_id}" > tests.log && cat tests.log
-exit "${tests_exit_code}"
+    # by container_id since the container is dead
+    docker logs --timestamps "${tests_container_id}" > tests.log && cat tests.log
+    exit "${tests_exit_code}"
+fi
