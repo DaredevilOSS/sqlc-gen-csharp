28d834056f2cbb1fb23c4ded75e0b03aac65be6d
Doron Eli Rachman <doron050@gmail.com>
2024-05-09 21:00:14 +0300

    test

--- scripts/update_wasm_plugin.sh
+++ scripts/update_wasm_plugin.sh
@@ -2,7 +2,11 @@
 
 set -e
 
-SOURCE_WASM_FILE="SqlcGenCsharpWasm/bin/Release/net8.0/wasi-wasm/AppBundle/SqlcGenCsharpWasm.wasm"
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+    SOURCE_WASM_FILE="SqlcGenCsharpWasm/obj/release/net8.0/wasi-wasm/wasm/for-publish/SqlcGenCsharpWasm.wasm"
+else
+    SOURCE_WASM_FILE="SqlcGenCsharpWasm/bin/Release/net8.0/wasi-wasm/AppBundle/SqlcGenCsharpWasm.wasm"
+fi
 cp "${SOURCE_WASM_FILE}" dist/plugin.wasm
 echo "WASM filesize:" && du -sh dist/plugin.wasm
 
