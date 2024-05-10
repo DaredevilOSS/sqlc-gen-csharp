be87784c863d383f0a41b2a54c105bc155c5d6fc
Doron Eli Rachman <doron050@gmail.com>
2024-05-10 03:42:58 +0300

    add tests on github actions

--- scripts/update_wasm_plugin.sh
+++ scripts/update_wasm_plugin.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
-set -e
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+    WASM_FILE="${SOURCE_WASM_FILE_UBUNTU}"
+    # need to install yq here for all operation systems
+else
+    set -e
 
-SOURCE_WASM_FILE="SqlcGenCsharpWasm/bin/Release/net8.0/wasi-wasm/AppBundle/SqlcGenCsharpWasm.wasm"
-cp "${SOURCE_WASM_FILE}" dist/plugin.wasm
+    source .env
+    WASM_FILE="${SOURCE_WASM_FILE}"
+fi
+echo "WASM_FILE = ${WASM_FILE}"
+mkdir -p dist
+cp "${WASM_FILE}" dist/plugin.wasm
 echo "WASM filesize:" && du -sh dist/plugin.wasm
 
 PLUGIN_SHA=$(shasum -a 256 dist/plugin.wasm | awk '{ print $1 }')
