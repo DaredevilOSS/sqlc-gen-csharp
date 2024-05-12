afb193cfebbc85634f50ebdf02b0f65d943fd8d2
Ilan Uzan <ilan9uzan@gmail.com>
2024-05-12 19:10:13 +0300

    delete DebugHelper project

--- SqlcGenCsharp/PluginRunner.cs
+++ SqlcGenCsharp/PluginRunner.cs
@@ -8,7 +8,6 @@ public static class PluginRunner
     public static void Run()
     {
         var generateRequest = ReadInput();
-        DebugHelper.Instance.Append("starting");
         var codeGenerator = new CodeGenerator(generateRequest);
         var generateResponse = codeGenerator.Generate();
         WriteOutput(generateResponse);
@@ -16,7 +15,6 @@ public static class PluginRunner
 
     private static GenerateRequest ReadInput()
     {
-        DebugHelper.Instance.Append("reading input");
         using var memoryStream = new MemoryStream();
         Console.OpenStandardInput().CopyTo(memoryStream);
         memoryStream.Position = 0;
@@ -26,7 +24,6 @@ public static class PluginRunner
 
     private static void WriteOutput(GenerateResponse output)
     {
-        DebugHelper.Instance.Append("writing output");
         var encodedOutput = output.ToByteArray();
         using var stdout = Console.OpenStandardOutput();
         stdout.Write(encodedOutput, 0, encodedOutput.Length);
