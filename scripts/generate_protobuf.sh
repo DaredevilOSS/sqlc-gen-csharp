c4c4c23ce285a488d37b1168ad6048912ab40301
Ilan Uzan <ilan9uzan@gmail.com>
2024-05-16 11:27:17 +0300

    fix generate protobuf script

--- scripts/generate_protobuf.sh
+++ scripts/generate_protobuf.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ "$(wget -q --spider http://google.com)" ]; then
+if wget -q --spider http://google.com; then
   buf generate --template buf.gen.yaml buf.build/sqlc/sqlc --path plugin/
 else
   echo "No internet connection - using pre-existing protobuf files.."
