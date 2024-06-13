3718952f5b6975e919e5021f02260942ab53af54
Ilan Uzan <ilan9uzan@gmail.com>
2024-06-13 18:41:56 +0300

    split ci job to build and test

--- scripts/update_config.sh
+++ scripts/update_config.sh
@@ -2,6 +2,9 @@
 
 set -ex
 
-file_per_query=$1
-yq -i ".sql[0].codegen[0].options.filePerQuery = ${file_per_query}" sqlc.wasm.yaml
-yq -i ".sql[1].codegen[0].options.filePerQuery = ${file_per_query}" sqlc.wasm.yaml
+filename=$1
+file_per_query=$2
+yq -i "
+  .sql[0].codegen[0].options.filePerQuery = ${file_per_query} |
+  .sql[1].codegen[0].options.filePerQuery = ${file_per_query}
+" "$filename"
