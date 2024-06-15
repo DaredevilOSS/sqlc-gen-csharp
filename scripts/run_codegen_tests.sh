aa31990be5f1552e4f4518cb339ade689a98c77c
Ilan Uzan <ilan9uzan@gmail.com>
2024-06-15 18:42:03 +0300

    test generate-csproj config

--- scripts/run_codegen_tests.sh
+++ scripts/run_codegen_tests.sh
@@ -1,23 +1,65 @@
 #!/usr/bin/env bash
 
-file_per_query=$1
-mysql_file_count=$(find MySqlConnectorExample/ -maxdepth 1 -name "*.cs" 2>/dev/null | wc -l)
+declare -a examples=("MySqlConnectorExample" "NpgsqlExample")
 
-if [ "${file_per_query}" = "true" ]; then
-    if [ "${mysql_file_count}" -gt 2 ]; then
-        echo "Assertion passed: ${mysql_file_count} > 2 .cs files are present in the directory MySqlConnectorExample."
-    else
-        echo "Assertion failed: ${mysql_file_count} <= 2 .cs files in the directory MySqlConnectorExample."
-        exit 1
-    fi
-elif [ "${file_per_query}" = "false" ]; then
-    if [ "${mysql_file_count}" -eq 2 ]; then
-        echo "Assertion passed: Exactly 2 .cs files are present in the directory MySqlConnectorExample."
-    else
-        echo "Assertion failed: ${mysql_file_count} != 2 .cs files in the directory MySqlConnectorExample."
-        exit 1
-    fi
-else
-    echo "Invalid input. Please provide 'true' or 'false'."
+remove_generated_files() {
+    generate_csproj=$1
+    for example_dir in "${examples[@]}"
+    do
+        echo "Deleting .cs files" && rm "${example_dir}/*.cs"
+        if [ "${generate_csproj}" = "true" ]; then
+            echo "Deleting .csproj file" && rm "${example_dir}/${example_dir}.csproj"
+        fi
+    done
+}
+
+check_cs_file_count() {
+    file_per_query=$1
+    for example_dir in "${examples[@]}"
+    do
+      file_count=$(find "${example_dir}/" -maxdepth 1 -name "*.cs" 2>/dev/null | wc -l)
+      if [ "${file_per_query}" = "true" ]; then
+          if [ "${file_count}" -gt 2 ]; then
+              echo "Assertion passed: More than 2 .cs files are present in the directory ${example_dir}."
+              return 0
+          else
+              echo "Assertion failed: Not more than 2 .cs files in the directory ${example_dir}."
+              return 1
+          fi
+      else
+          if [ "${file_count}" -eq 2 ]; then
+              echo "Assertion passed: Exactly 2 .cs files are present in the directory ${example_dir}."
+              return 0
+          else
+              echo "Assertion failed: Not exactly 2 .cs files in the directory ${example_dir}."
+              return 1
+          fi
+      fi
+    done
+}
+
+check_csproj_file() {
+    generate_csproj=$1
+    for example_dir in "${examples[@]}"
+    do
+        if [ -f "${example_dir}/${example_dir}.csproj" ]; then
+            echo "Assertion failed: A .csproj file is present in the directory ${example_dir}."
+            return 1
+        fi
+    done
+}
+
+file_per_query=$1
+generate_csproj=$2
+
+remove_generated_files "${generate_csproj}"
+sqlc -f sqlc.ci.yaml generate
+
+status_code=$(check_cs_file_count "${file_per_query}")
+if [ "${status_code}" -ne 0 ]; then
+    exit 1
+fi
+status_code=$(check_csproj_file "${generate_csproj}")
+if [ "${status_code}" -ne 0 ]; then
     exit 1
 fi
--- scripts/update_config.sh
+++ scripts/update_config.sh
@@ -4,7 +4,10 @@ set -ex
 
 filename=$1
 file_per_query=$2
+generate_csproj=$3
 yq -i "
   .sql[0].codegen[0].options.filePerQuery = ${file_per_query} |
-  .sql[1].codegen[0].options.filePerQuery = ${file_per_query}
+  .sql[1].codegen[0].options.filePerQuery = ${file_per_query} |
+  .sql[0].codegen[0].options.generateCsproj = ${generate_csproj} |
+  .sql[1].codegen[0].options.generateCsproj = ${generate_csproj}
 " "$filename"
