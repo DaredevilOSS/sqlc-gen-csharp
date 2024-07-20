48bdb3b4f69f41320b51a5bf75ee6eb7b688c8c4
Ilan Uzan <ilan9uzan@gmail.com>
2024-07-20 11:02:08 +0300

    standardize list construction across project

--- Extensions/ListExtensions.cs
+++ Extensions/ListExtensions.cs
@@ -7,6 +7,11 @@ public static class ListExtensions
         return condition ? me.Append(item) : me;
     }
 
+    public static IEnumerable<T> AppendIfNotNull<T>(this IEnumerable<T> me, T? item)
+    {
+        return item is not null ? me.Append(item) : me;
+    }
+
     public static string JoinByNewLine(this IEnumerable<string> me)
     {
         return string.Join("\n", me);
