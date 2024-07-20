077e21aaa998f7a866b012e0000f6fddd9716506
Ilan Uzan <ilan9uzan@gmail.com>
2024-07-20 15:19:15 +0300

    standardize list construction across project (#71)

--- Drivers/Generators/ManyDeclareGen.cs
+++ Drivers/Generators/ManyDeclareGen.cs
@@ -13,9 +13,10 @@ public class ManyDeclareGen(DbDriver dbDriver)
     public MemberDeclarationSyntax Generate(string funcName, string queryTextConstant, string argInterface,
         string returnInterface, IList<Parameter> parameters, IEnumerable<Column> columns)
     {
+        var parameterList = CommonGen.GetParameterListAsString(argInterface, parameters);
         var returnType = $"Task<List<{returnInterface}>>";
         return ParseMemberDeclaration($$"""
-                                        public async {{returnType}} {{funcName}}({{CommonGen.GetParameterListAsString(argInterface, parameters)}})
+                                        public async {{returnType}} {{funcName}}({{parameterList}})
                                         {
                                             {{GetMethodBody(queryTextConstant, returnInterface, columns, parameters)}}
                                         }
@@ -30,10 +31,11 @@ public class ManyDeclareGen(DbDriver dbDriver)
         var commandParameters = CommonGen.GetCommandParameters(parameters);
         var initDataReader = CommonGen.InitDataReader();
         var awaitReaderRow = CommonGen.AwaitReaderRow();
+        var dataclassInit = CommonGen.InstantiateDataclass(columns, returnInterface);
         var readWhileExists = $$"""
                                 while ({{awaitReaderRow}})
                                 {
-                                    {{Variable.Result.Name()}}.Add({{CommonGen.InstantiateDataclass(columns, returnInterface)}});
+                                    {{Variable.Result.Name()}}.Add({{dataclassInit}});
                                 }
                                 """;
 
