c6e63e3df3f6ce84d9ab118d5a30ea343500c6e4
Ilan Uzan <ilan9uzan@gmail.com>
2024-12-24 23:56:39 +0100

    tmp

--- scripts/tests/run_end2end.sh
+++ scripts/tests/run_end2end.sh
@@ -4,9 +4,11 @@ set -ex
 
 if [ "$GITHUB_ACTIONS" = "true" ]; then
     echo "Running in Github Actions"
+    
     core_tests_id=$(docker ps -aqf "name=${TESTS_DOTNET_CORE_CONTAINER}")
     core_exit_code=$(docker wait "${TESTS_DOTNET_CORE_CONTAINER}")
     docker logs --timestamps "${core_tests_id}"
+    
     framework_tests_id=$(docker ps -aqf "name=${TESTS_DOTNET_FRAMEWORK_CONTAINER}")
     framework_exit_code=$(docker wait "${TESTS_DOTNET_FRAMEWORK_CONTAINER}")
     docker logs --timestamps "${framework_tests_id}"
