ee68db082a2114d4e2e503b9ae2b7bb63e843550
Ilan Uzan <ilan9uzan@gmail.com>
2024-12-28 19:24:41 +0100

    1. fix readme quickstart (#127)
    2. have the README auto-generated via pandoc

---
+++ scripts/docs/generate_quickstart.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+set -e
+
+doc_file="docs/$1"
+plugin_version=$(git tag | sort --version-sort | tail -n1)
+plugin_url="https://github.com/DaredevilOSS/sqlc-gen-csharp/releases/download/${plugin_version}/sqlc-gen-csharp.wasm"
+release_sha=$(curl -s "${plugin_url}" | shasum -a 256 | awk '{ print $1 }')
+    
+contents="# Quickstart
+## Configuration
+\`\`\`yaml
+version: \"2\"
+plugins:
+- name: csharp
+  wasm:
+    url: https://github.com/DaredevilOSS/sqlc-gen-csharp/releases/download/${plugin_version}/sqlc-gen-csharp.wasm
+    sha256: ${release_sha}
+sql:
+  # For PostgresSQL
+  - schema: schema.sql
+    queries: queries.sql
+    engine: postgresql
+    codegen:
+      - plugin: csharp
+        out: PostgresDalGen
+  # For MySQL
+  - schema: schema.sql
+    queries: queries.sql
+    engine: mysql
+    codegen:
+      - plugin: csharp
+        out: MySqlDalGen
+  # For SQLite
+  - schema: schema.sql
+    queries: queries.sql
+    engine: sqlite
+    codegen:
+      - plugin: csharp
+        out: SqliteDalGen
+\`\`\`
+"
+
+echo "${contents}" > "${doc_file}"
