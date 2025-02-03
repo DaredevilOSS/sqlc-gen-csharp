b7dc6a9c87a74f19768943ab55d8edf0d69f9f22
Doron Eli Rachman <doron050@gmail.com>
2025-02-03 23:22:07 +0200

    Add tests for slice functionality in MySqlConnectorDapperTester

--- end2end/EndToEndTestsLegacy/MySqlConnectorDapperTester.cs
+++ end2end/EndToEndTestsLegacy/MySqlConnectorDapperTester.cs
@@ -1,5 +1,6 @@
 using MySqlConnectorDapperLegacyExampleGen;
 using NUnit.Framework;
+using NUnit.Framework.Legacy;
 using System;
 using System.Threading.Tasks;
 
@@ -15,6 +16,54 @@ namespace SqlcGenCsharpTests
         public async Task EmptyTestsTable()
         {
             await QuerySql.TruncateAuthors();
+        }
+
+        [Test]
+        public async Task TestSliceIds()
+        {
+            var insertedId1 = await QuerySql.CreateAuthorReturnId(new QuerySql.CreateAuthorReturnIdArgs
+            {
+                Name = DataGenerator.GenericAuthor,
+                Bio = DataGenerator.GenericQuote1
+            });
+            var insertedId2 = await QuerySql.CreateAuthorReturnId(new QuerySql.CreateAuthorReturnIdArgs
+            {
+                Name = DataGenerator.GenericAuthor,
+                Bio = DataGenerator.GenericQuote1
+            });
+            await QuerySql.CreateAuthor(new QuerySql.CreateAuthorArgs
+            {
+                Name = DataGenerator.GenericAuthor,
+                Bio = DataGenerator.GenericQuote1
+            });
+
+            var actual = await QuerySql.SelectAuthorsWithSlice(new QuerySql.SelectAuthorsWithSliceArgs { Ids = [insertedId1, insertedId2] });
+
+            ClassicAssert.AreEqual(2, actual.Count);
+        }
+
+        [Test]
+        public async Task TestTwoSlices()
+        {
+            var insertedId1 = await QuerySql.CreateAuthorReturnId(new QuerySql.CreateAuthorReturnIdArgs
+            {
+                Name = DataGenerator.GenericAuthor,
+                Bio = DataGenerator.GenericQuote1
+            });
+            var insertedId2 = await QuerySql.CreateAuthorReturnId(new QuerySql.CreateAuthorReturnIdArgs
+            {
+                Name = DataGenerator.BojackAuthor,
+                Bio = DataGenerator.GenericQuote1
+            });
+            await QuerySql.CreateAuthor(new QuerySql.CreateAuthorArgs
+            {
+                Name = DataGenerator.GenericAuthor,
+                Bio = DataGenerator.GenericQuote1
+            });
+
+            var actual = await QuerySql.SelectAuthorsWithTwoSlices(new QuerySql.SelectAuthorsWithTwoSlicesArgs { Ids = [insertedId1, insertedId2], Names = [DataGenerator.GenericAuthor] });
+
+            ClassicAssert.AreEqual(1, actual.Count);
         }
     }
 }
