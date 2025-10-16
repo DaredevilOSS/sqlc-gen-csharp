bdc1aed783f2dfb91d1ad9ddd23a772d73e42099
wleev <wlee@isabot.net>
2025-10-16 11:05:38 +0900

    Net9.0 is .NET Core

--- PluginOptions/DotnetFramework.cs
+++ PluginOptions/DotnetFramework.cs
@@ -7,13 +7,15 @@ public enum DotnetFramework
 {
     Dotnet80,
     DotnetStandard20,
-    DotnetStandard21
+    DotnetStandard21,
+    Dotnet90
 }
 
 public static class DotnetFrameworkExtensions
 {
     private static readonly Dictionary<DotnetFramework, string> EnumToString = new()
     {
+        { DotnetFramework.Dotnet90, "net9.0" },
         { DotnetFramework.Dotnet80, "net8.0" },
         { DotnetFramework.DotnetStandard21, "netstandard2.1" },
         { DotnetFramework.DotnetStandard20, "netstandard2.0" }
@@ -26,14 +28,16 @@ public static class DotnetFrameworkExtensions
 
     public static DotnetFramework ParseName(string dotnetFramework)
     {
-        return EnumToString
-            .ToDictionary(x => x.Value, x => x.Key)
-            [dotnetFramework];
+        return EnumToString.ToDictionary(x => x.Value, x => x.Key)[dotnetFramework];
     }
 
     public static bool IsDotnetCore(this DotnetFramework me)
     {
-        return me == DotnetFramework.Dotnet80;
+        return new List<DotnetFramework>
+        {
+            DotnetFramework.Dotnet80,
+            DotnetFramework.Dotnet90,
+        }.Contains(me);
     }
 
     public static bool IsDotnetLegacy(this DotnetFramework me)
