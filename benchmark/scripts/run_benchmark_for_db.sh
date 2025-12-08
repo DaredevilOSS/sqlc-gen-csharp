8d223a72a2dd9c89cf896441514c8744a3f3b447
Ilan Uzan <ilan9uzan@gmail.com>
2025-12-09 00:11:20 +0100

    fix: benchmarks

--- benchmark/scripts/run_benchmark_for_db.sh
+++ benchmark/scripts/run_benchmark_for_db.sh
@@ -7,12 +7,12 @@ dotnet build ./benchmark/BenchmarkRunner/BenchmarkRunner.csproj -c Release
 
 if [ "$GITHUB_ACTIONS" = "true" ]; then
     echo "Running in Github Actions"
-    dotnet run -c Release --no-build --project ./BenchmarkRunner/BenchmarkRunner.csproj -- --database $database_to_benchmark
+    dotnet run -c Release --no-build --project ./benchmark/BenchmarkRunner/BenchmarkRunner.csproj -- --database $database_to_benchmark
 else
     destroy() { docker-compose down --volumes; }
     trap destroy EXIT
     
     echo "Running in local"
     docker-compose up --build --detach --force-recreate --remove-orphans --wait
-    dotnet run -c Release --no-build --project ./BenchmarkRunner/BenchmarkRunner.csproj -- --database $database_to_benchmark
+    dotnet run -c Release --no-build --project ./benchmark/BenchmarkRunner/BenchmarkRunner.csproj -- --database $database_to_benchmark
 fi
