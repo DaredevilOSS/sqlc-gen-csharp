44927fda5d290a58c8ac03f204081a30573a67dd
Ilan Uzan <ilan9uzan@gmail.com>
2025-12-09 11:20:08 +0100

    fix: dotnet run command

--- benchmark/scripts/run_benchmark_for_db.sh
+++ benchmark/scripts/run_benchmark_for_db.sh
@@ -6,12 +6,12 @@ database_to_benchmark=$1
 
 if [ "$GITHUB_ACTIONS" = "true" ]; then
     echo "Running in Github Actions"
-    dotnet run -c Release --no-build --project ./benchmark/BenchmarkRunner/BenchmarkRunner.csproj -- --database $database_to_benchmark
+    dotnet run -c Release --project ./benchmark/BenchmarkRunner/BenchmarkRunner.csproj -- --database $database_to_benchmark
 else
     destroy() { docker-compose down --volumes; }
     trap destroy EXIT
     
     echo "Running in local"
     docker-compose up --build --detach --force-recreate --remove-orphans --wait
-    dotnet run -c Release --no-build --project ./benchmark/BenchmarkRunner/BenchmarkRunner.csproj -- --database $database_to_benchmark
+    dotnet run -c Release --project ./benchmark/BenchmarkRunner/BenchmarkRunner.csproj -- --database $database_to_benchmark
 fi
