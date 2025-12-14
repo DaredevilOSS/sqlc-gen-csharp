e30d12fd37b510b5b2ac53abbd0fa05333d5f45b
Ilan Uzan <ilan9uzan@gmail.com>
2025-12-14 17:15:50 +0100

    fix: accesibility of members

--- benchmark/BenchmarkRunner/Benchmarks/ReadBenchmark.cs
+++ benchmark/BenchmarkRunner/Benchmarks/ReadBenchmark.cs
@@ -6,10 +6,10 @@ public abstract class ReadBenchmark
     protected SemaphoreSlim _initLock = new(1, 1);
 
     [Params(1000)]
-    protected int QueriesToRun { get; set; }
+    public int QueriesToRun { get; set; }
 
     [Params(500)]
-    protected int CustomerCount { get; set; }
+    public int CustomerCount { get; set; }
 
     [Params(100, 500)]
     public int Limit { get; set; }
--- benchmark/BenchmarkRunner/Benchmarks/WriteBenchmark.cs
+++ benchmark/BenchmarkRunner/Benchmarks/WriteBenchmark.cs
@@ -6,7 +6,7 @@ public abstract class WriteBenchmark
     protected SemaphoreSlim _initLock = new(1, 1);
 
     [Params(2000000)]
-    protected int TotalRecords { get; set; }
+    public int TotalRecords { get; set; }
 
     public abstract Task Sqlc_AddOrderItems();
     public abstract Task EFCore_AddOrderItems();
